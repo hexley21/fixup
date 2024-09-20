c197a7f94178c3050fdc65e6745e3624a3975f16
Hexley21 <a.kupreishvili.21@gmail.com>
2024-09-20 20:06:40 +0400

    test: add rbac middleware tests

--- internal/common/rest/errors.go
+++ internal/common/rest/errors.go
@@ -11,16 +11,20 @@ const (
 	MsgUserNotFound      = "User not found"
 	MsgUserAlreadyExists = "User already exists"
 
+	MsgInsufficientRights = "Insufficient rights"
+	MsgUserIsVerified     = "User has to be not-verified"
+	MsgUserIsNotVerified  = "User is not verified"
+
 	MsgIncorrectPassword    = "Password is incorrect"
 	MsgIncorrectEmailOrPass = "Email or Password is incorrect"
 
 	MsgFileReadError = "Failed read file"
-	MsgNoFile = "No file provided"
-	MsgTooManyFiles = "Too many files"
+	MsgNoFile        = "No file provided"
+	MsgTooManyFiles  = "Too many files"
 
-	MsgInvalidToken = "Invalid token"
+	MsgInvalidToken               = "Invalid token"
 	MsgMissingAuthorizationHeader = "Authorization header is missing"
-	MsgMissingBearerToken = "Bearer token is missing"
+	MsgMissingBearerToken         = "Bearer token is missing"
 
 	MsgInternalServerError = "Something went wrong"
 )
